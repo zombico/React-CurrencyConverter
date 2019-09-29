0c46e9b52ec40fa8bda0c0c8fbca8ae9cc2b3ba6
zombico <franz@ombi.co>
2019-09-29 01:03:27 -0400

    prod latest

---
+++ build/precache-manifest.2f9a91d04b115bbaadabfba8c31ed59f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "690c71a9d4ea3b728ac2f12dfd2bc9ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e84fa1bbc3bbed4b1da",
+    "url": "/static/css/main.e9f922e6.chunk.css"
+  },
+  {
+    "revision": "9aff4057e1fb0225e732",
+    "url": "/static/js/2.735db684.chunk.js"
+  },
+  {
+    "revision": "9e84fa1bbc3bbed4b1da",
+    "url": "/static/js/main.fda6b9ed.chunk.js"
+  },
+  {
+    "revision": "1b8a38af9ea66841f7d0",
+    "url": "/static/js/runtime~main.35e32561.js"
+  }
+]);
